e17c70160097f443705f9e5c209f8f194bed11be
DeeJayJay <deejayjay@zoho.com>
2024-12-20 23:33:54 -0700

    Completes section-3: The Vue CLI

---
+++ section-3/modal-project/src/App.vue
@@ -0,0 +1,76 @@
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: "My First Vue App",
+      modalVisible: false,
+      modalTwoVisible: false
+    };
+  },
+  methods: {
+    showModal() {
+      this.modalVisible = true;
+    },
+    hideModal() {
+      this.modalVisible = false;
+    },
+    showModalTwo() {
+      this.modalTwoVisible = true;
+    },
+    hideModalTwo() {
+      this.modalTwoVisible = false;
+    }
+  }
+};
+</script>
+
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+
+  <main>
+    <button type="button"
+            @click="showModal">Show Modal 1</button>
+    <button type="button"
+            @click="showModalTwo">Show Modal 2</button>
+    <Teleport to="#modals"
+              v-if="modalVisible">
+      <Modal theme="sale"
+             @on-close="hideModal">
+        <h2>Ninja Giveaway!</h2>
+        <p>Grab your ninja swag for half the price!</p>
+        <template v-slot:links>
+          <a href="#">Sign up</a>
+          <a href="#">More info</a>
+        </template>
+      </Modal>
+    </Teleport>
+
+    <Teleport to="#modals"
+              v-if="modalTwoVisible">
+      <Modal @on-close="hideModalTwo">
+        <h2>Another Modal</h2>
+        <template #links>
+          <a href="#">Close</a>
+        </template>
+      </Modal>
+    </Teleport>
+  </main>
+</template>
+
+<style scoped>
+h2 {
+  margin-bottom: 1rem;
+}
+
+p {
+  margin-bottom: 1.5rem;
+}
+</style>
---
+++ section-3/modal-project/src/components/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="backdrop"
+       @click.self="closeModal">
+    <div class="modal"
+         :class="{ sale: theme === 'sale' }">
+      <slot></slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    theme: {
+      type: String
+    }
+  },
+  methods: {
+    closeModal() {
+      this.$emit("onClose");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.backdrop {
+  position: fixed;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.modal {
+  width: min(30rem, 100%);
+  margin: 6rem auto 1.5rem;
+  background: #fff;
+  border-radius: 0.625rem;
+  padding: 1.5rem;
+}
+
+.sale {
+  background-color: #8acece;
+}
+</style>
